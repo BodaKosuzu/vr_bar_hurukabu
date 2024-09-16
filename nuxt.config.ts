c40c0a5738c18cfad754731b4f6ac900851f6515
BodaKosuzu <ethyrene@gmail.com>
2024-09-16 15:01:20 +0900

    初期構築

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   app: {
-    baseURL: 'vr_bar_hurukabu'
+    baseURL: '/vr_bar_hurukabu/'
   }
 })
